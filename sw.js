217b4e2b9fe2fe2e98ed21c4c85e2503cd832835
Galileo <tavishberlin@outlook.com>
2026-09-21 08:15:30 -0500

    clean up root

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME='hls-pwa-v101';
+const CACHE_NAME='hls-pwa-v102';
 const CORE=[
   './',
   './index.html',
   './manifest.webmanifest',
-  './favicon.png',
-  './hls_logo.png',
+  './icons/favicon.png',
+  './images/hls_logo.png',
   './icons/icon-192.png',
   './icons/icon-512.png',
   './icons/icon-maskable-512.png',
